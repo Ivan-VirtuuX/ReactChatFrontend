7c7c3614869f65da78c2e40622a50b12c791f3a7
VirtuuX <somargame22@gmail.com>
2023-01-02 01:51:30 +0600

    commit

--- utils/context/SocketContext.ts
+++ utils/context/SocketContext.ts
@@ -1,6 +1,6 @@
 import { createContext } from 'react';
 import { io } from 'socket.io-client';
 
-export const socket = io('https://reactchatbackend.onrender.com:8888');
+export const socket = io(`https://reactchatbackend.onrender.com:${process.env.PORT}`);
 
 export const SocketContext = createContext(socket);
